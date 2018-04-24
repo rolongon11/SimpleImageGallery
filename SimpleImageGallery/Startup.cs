ce8a485b6898d620c7a1bc9d83be37714e513d12
rolongon11 <rolongon@hotmail.com>
2018-04-23 19:29:39 -0600

    Initial Commit

--- SimpleImageGallery/Startup.cs
+++ SimpleImageGallery/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SimpleImageGallery.Data;
 
 namespace SimpleImageGallery
 {
@@ -21,6 +23,10 @@ namespace SimpleImageGallery
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SimpleImageGalleryDbContext>(options => 
+            options.UseSqlServer(Configuration
+            .GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
